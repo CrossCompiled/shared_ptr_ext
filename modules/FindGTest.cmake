15e56a14e10709096ad4b11e604d51866d769ab3
Kalle Møller <github.com@k-moeller.dk>
2016-08-31 21:16:02 +0200

    Might work

--- modules/FindGTest.cmake
+++ modules/FindGTest.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 ExternalProject_Add(
         googletest
-        URL https://github.com/google/googletest/archive/release-1.8.0.zip
+        URL https://github.com/google/googletest/archive/release-1.7.0.zip
         # TIMEOUT 10
         # # Force separate output paths for debug and release builds to allow easy
         # # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
@@ -20,9 +20,14 @@ ExternalProject_Get_Property(googletest source_dir)
 ExternalProject_Get_Property(googletest binary_dir)
 
 set(GTEST_FOUND        TRUE)
-set(GTEST_INCLUDE_DIR  ${source_dir}/googletest/include)
-set(GTEST_LIBRARY      ${binary_dir}/googlemock/gtest/libgtest.a)
-set(GTEST_MAIN_LIBRARY ${binary_dir}/googlemock/gtest/libgtest_main.a)
+set(GTEST_INCLUDE_DIR  ${source_dir}/include)
+#set(GTEST_LIBRARY      ${binary_dir}/googlemock/gtest/libgtest.a)
+set(GTEST_LIBRARY      ${binary_dir}/libgtest.a)
+#set(GTEST_MAIN_LIBRARY ${binary_dir}/googlemock/gtest/libgtest_main.a)
+set(GTEST_MAIN_LIBRARY ${binary_dir}/libgtest_main.a)
+
+set(GTEST_LIBRARIES GTest::GTest)
+set(GTEST_MAIN_LIBRARIES GTest::Main)
 
 if(GTEST_FOUND)
     set(GTEST_INCLUDE_DIRS ${GTEST_INCLUDE_DIR})
@@ -30,31 +35,27 @@ if(GTEST_FOUND)
     include(CMakeFindDependencyMacro)
     find_dependency(Threads)
 
-    if(NOT TARGET GTest::GTest)
-        add_library(GTest::GTest UNKNOWN IMPORTED)
-        set_target_properties(GTest::GTest PROPERTIES
+    if(NOT TARGET ${GTEST_LIBRARIES})
+        add_library(${GTEST_LIBRARIES} UNKNOWN IMPORTED)
+        set_target_properties(${GTEST_LIBRARIES} PROPERTIES
                 INTERFACE_LINK_LIBRARIES "Threads::Threads")
-        if(EXISTS "${GTEST_LIBRARY}")
-            set_target_properties(GTest::GTest PROPERTIES
+       set_target_properties(${GTEST_LIBRARIES} PROPERTIES
                     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
                     IMPORTED_LOCATION "${GTEST_LIBRARY}")
-        endif()
-        add_dependencies(GTest::GTest googletest)
+        add_dependencies(${GTEST_LIBRARIES} googletest)
 
     endif()
-    if(NOT TARGET GTest::Main)
-        add_library(GTest::Main UNKNOWN IMPORTED)
-        set_target_properties(GTest::Main PROPERTIES
-                INTERFACE_LINK_LIBRARIES "GTest::GTest")
-        if(EXISTS "${GTEST_MAIN_LIBRARY}")
-            set_target_properties(GTest::Main PROPERTIES
+    if(NOT TARGET ${GTEST_MAIN_LIBRARIES})
+        add_library(${GTEST_MAIN_LIBRARIES} UNKNOWN IMPORTED)
+        set_target_properties(${GTEST_MAIN_LIBRARIES} PROPERTIES
+                INTERFACE_LINK_LIBRARIES "${GTEST_LIBRARIES}")
+       set_target_properties(${GTEST_MAIN_LIBRARIES} PROPERTIES
                     IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
                     IMPORTED_LOCATION "${GTEST_MAIN_LIBRARY}")
-        endif()
-        add_dependencies(GTest::Main googletest)
+        add_dependencies(${GTEST_MAIN_LIBRARIES} googletest)
     endif()
 
-    set(GTEST_LIBRARIES GTest::GTest)
-    set(GTEST_MAIN_LIBRARIES GTest::Main)
+
+
     set(GTEST_BOTH_LIBRARIES ${GTEST_LIBRARIES} ${GTEST_MAIN_LIBRARIES})
 endif()
