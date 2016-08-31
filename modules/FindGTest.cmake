c4a75c93e601602fc6e28d2aaaff7d7d6595e8e4
Kalle Møller <github.com@k-moeller.dk>
2016-08-31 21:50:05 +0200

    ctest

--- modules/FindGTest.cmake
+++ modules/FindGTest.cmake
@@ -1,4 +1,5 @@
 include(ExternalProject)
+
 ExternalProject_Add(
         googletest
         URL https://github.com/google/googletest/archive/release-1.7.0.zip
@@ -15,6 +16,39 @@ ExternalProject_Add(
         LOG_CONFIGURE ON
         LOG_BUILD ON
 )
+
+function(GTEST_ADD_TESTS executable extra_args)
+    if(NOT ARGN)
+        message(FATAL_ERROR "Missing ARGN: Read the documentation for GTEST_ADD_TESTS")
+    endif()
+    if(ARGN STREQUAL "AUTO")
+        # obtain sources used for building that executable
+        get_property(ARGN TARGET ${executable} PROPERTY SOURCES)
+    endif()
+    set(gtest_case_name_regex ".*\\( *([A-Za-z_0-9]+) *, *([A-Za-z_0-9]+) *\\).*")
+    set(gtest_test_type_regex "(TYPED_TEST|TEST_?[FP]?)")
+    foreach(source ${ARGN})
+        set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${source})
+        file(READ "${source}" contents)
+        string(REGEX MATCHALL "${gtest_test_type_regex} *\\(([A-Za-z_0-9 ,]+)\\)" found_tests ${contents})
+        foreach(hit ${found_tests})
+            string(REGEX MATCH "${gtest_test_type_regex}" test_type ${hit})
+
+            # Parameterized tests have a different signature for the filter
+            if("x${test_type}" STREQUAL "xTEST_P")
+                string(REGEX REPLACE ${gtest_case_name_regex}  "*/\\1.\\2/*" test_name ${hit})
+            elseif("x${test_type}" STREQUAL "xTEST_F" OR "x${test_type}" STREQUAL "xTEST")
+                string(REGEX REPLACE ${gtest_case_name_regex} "\\1.\\2" test_name ${hit})
+            elseif("x${test_type}" STREQUAL "xTYPED_TEST")
+                string(REGEX REPLACE ${gtest_case_name_regex} "\\1/*.\\2" test_name ${hit})
+            else()
+                message(WARNING "Could not parse GTest ${hit} for adding to CTest.")
+                continue()
+            endif()
+            add_test(NAME ${test_name} COMMAND ${executable} --gtest_filter=${test_name} ${extra_args})
+        endforeach()
+    endforeach()
+endfunction()
 
 ExternalProject_Get_Property(googletest source_dir)
 ExternalProject_Get_Property(googletest binary_dir)
